28834054ca5013638ee0e0c3d47ba1d73cc7a0a3
Nico <nicoc12024@gmail.com>
2022-11-09 16:56:58 +0100

    agregar productos page responsive

--- frontend/src/pages/AgregarProducto.jsx
+++ frontend/src/pages/AgregarProducto.jsx
@@ -29,71 +29,75 @@ function AgregarProducto() {
 
   return (
     <>
-      <div className="flex items-center max-w-[370px] mx-auto py-4 ml-8">
-        <AiOutlineArrowLeft size={18} onClick={() => navigate("/")} />
-        <h2 className="font-medium uppercase ml-2 text-l">Agregar Nuevo Producto</h2>
+      <div className="mx-auto px-6 max-w-4xl">
+        <div className="flex items-center py-4">
+          <AiOutlineArrowLeft size={28} onClick={() => navigate("/")} />
+          <h2 className="font-medium uppercase w-full  md:text-2xl md:font-bold text-center text-l">
+            Agregar Nuevo Producto
+          </h2>
+        </div>
       </div>
-      <div className="px-6 flex flex-col gap-4 md:gap-8 justify-center md:flex-row">
-        <main className="w-full mt-5 overflow-hidden md:max-w-md rounded-lg">
+      <div className="px-6 flex flex-col gap-4 md:gap-8 justify-center mx-auto max-w-xl desktop:max-w-2xl">
+        <main className="w-full mt-5 overflow-hidden rounded-lg">
           <form onSubmit={onSubmit}>
             <div className="mb-4">
-              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700 undefined">
+              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700">
                 URL de la imagen
               </label>
-              <div className=" flex flex-col items-start">
+              <div>
                 <input
                   onChange={onMutate}
                   type="text"
                   id="url"
                   required
-                  className="bg-slate-200 p-2 block w-full mt-1 border-gray-300 rounded-sm shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  className="bg-slate-200 p-2 block w-full mt-1 rounded-sm shadow-sm"
                 />
               </div>
             </div>
             <div className="mb-4">
-              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700 undefined">
+              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700">
                 Categoría
               </label>
-              <div className=" flex flex-col items-start">
+              <div>
                 <input
                   onChange={onMutate}
                   type="text"
                   id="category"
                   required
-                  className="bg-slate-200 p-2 block w-full mt-1 border-gray-300 rounded-sm shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  className="bg-slate-200 p-2 block w-full mt-1 rounded-sm shadow-sm"
                 />
               </div>
             </div>
             <div className="mb-4">
-              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700 undefined">
+              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700">
                 Precio del producto
               </label>
-              <div className=" flex flex-col items-start">
+              <div>
                 <input
                   onChange={onMutate}
                   type="text"
                   id="price"
                   required
-                  className="bg-slate-200 p-2 block w-full mt-1 border-gray-300 rounded-sm shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  className="bg-slate-200 p-2 block w-full mt-1 rounded-sm shadow-sm"
                 />
               </div>
             </div>
             <div className="mb-4">
-              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700 undefined">
+              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700">
                 Nombre del producto
               </label>
-              <div className=" flex flex-col items-start">
+              <div>
                 <input
                   onChange={onMutate}
                   type="text"
                   id="name"
                   required
-                  className="bg-slate-200 p-2 block w-full mt-1 border-gray-300 rounded-sm shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  className="bg-slate-200 p-2 block w-full mt-1 rounded-sm shadow-sm"
                 />
               </div>
             </div>
             <div className="mb-4">
-              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700 undefined">
+              <label className="block mb-1 text-sm md:text-xl font-medium text-gray-700">
                 Descripción del producto
               </label>
               <div className="flex flex-col items-start">
@@ -101,7 +105,7 @@ function AgregarProducto() {
                   onChange={onMutate}
                   type="text"
                   id="description"
-                  className="min-h-40	 bg-slate-200 block w-full mt-1 border-gray-300 rounded-sm shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  className="min-h-40 bg-slate-200 block w-full mt-1 rounded-sm shadow-sm"
                 />
               </div>
             </div>
@@ -109,7 +113,7 @@ function AgregarProducto() {
             <div className="flex items-center justify-end mt-4">
               <button
                 type="submit"
-                className="bg-slate-500 w-full py-2 text-xs md:text-xl font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md active:bg-gray-900 false"
+                className="bg-slate-500 w-full py-2 text-xs md:text-xl font-semibold tracking-widest text-white uppercase rounded-md active:bg-gray-900"
               >
                 Agregar Producto
               </button>
