feb0e2859c139f8c0c75aaeca74fa6c0c6e6a037
Mariano Labatut <marianolabatut@gmail.com>
2022-11-11 10:08:28 -0300

    Navbar: scroll sticky

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="w-full bg-purple-500 shadow">
+    <nav className="w-full bg-purple-500 sticky top-0">
       <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
