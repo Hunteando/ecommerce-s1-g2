b2955a75e5797896b2b526673bb293e9cecde1ff
Andres1410 <camachorodriguezandresfelipe@gmail.com>
2022-11-15 18:11:45 -0500

    modal actualizado

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,79 +1,129 @@
-import { Fragment, useState } from "react";
-import { Button,Dialog,Card,CardHeader,CardBody,CardFooter,Typography,Input,Checkbox } from "@material-tailwind/react";
+import React from "react";
 
-import "../css/modal.css";
- 
+import { Button} from "@material-tailwind/react";
+
+import { LockClosedIcon } from '@heroicons/react/20/solid'
+
+
 export default function Modal() {
-  const [open, setOpen] = useState(false);
- 
-  const handleOpen = () => setOpen(!open);
- 
+  const [showModal, setShowModal] = React.useState(false);
   return (
-    <Fragment>
-      <Button onClick={handleOpen} >
-         <svg class="w-6 h-6 text-gray-200 hover:text-indigo-200 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <>
+      <Button
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        <svg class="w-6 h-6 text-gray-200 hover:text-indigo-200 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
             </svg>
       </Button>
 
-      <Dialog 
 
-        open={open}
-        handler={handleOpen}
+      {showModal ? (
 
-        animate={{
-          mount: { scale: 1, y: 0 },
-          unmount: { scale: 0.9, y: -100 },
-        }}
-
-        className="modal"
-      > 
-        
-        <Card className="w-80" >
-              
-              <CardHeader
-                variant="gradient"
-                color="#fff"
-                className="mb-4 grid h-28 place-items-center"
-              >
-                <Typography variant="h3" color="black">
-                  Usuari@
-                </Typography>
-              </CardHeader>
-
-              <CardBody className="flex flex-col gap-4 contedor">
-                <Input  size="lg" placeholder="correo electronico"/>
-                <Input type="password" class="form-control" size="lg" placeholder="pasword" />
-
-                <div className="-ml-2.5">
-                  <Checkbox label="Remember Me" />
+        <>
+         <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/} 
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <img
+                      className="mx-auto h-12 w-auto"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                      alt="Your Company"
+                    />
+                    <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                      Sign in to your account
+                    </h2>
                 </div>
 
-              </CardBody>
+                {/*body*/}
 
-              <CardFooter className="pt-0" color="black">
-                <Button  fullWidth>
-                  Ingresar
-                </Button>
-                <Typography variant="small" className="mt-6 flex justify-center">
-                  No tiene una cuenta?
+            <input type="hidden" name="remember" defaultValue="true" />
+                 <div className="-space-y-px rounded-md shadow-sm">
+                  <div>
+                  <label htmlFor="email-address" className="sr-only">
+                      Email address
+                    </label>
+                    <input
+                      id="email-address"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      placeholder="Email address"
+                    />
+                    </div>
 
-                  <Typography
-                    as="a"
-                    href="/Registro"
-                    variant="small"
-                    color="black"
-                    className="ml-1 font-bold"
+                    <div>
+                      <label htmlFor="password" className="sr-only">
+                        Password
+                      </label>
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Password"
+                      />
+              </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                  Olvide contraseña?
+                </a>
+              </div>
+            </div>
+
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                </span>
+                Ingresar
+              </button>
+            </div>
+                
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
                   >
-                    Registrate
-                  </Typography>
-
-                </Typography>
-
-              </CardFooter>
-            </Card>
-
-      </Dialog>
-    </Fragment>
+                    x
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
   );
 }
