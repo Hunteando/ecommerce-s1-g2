66ef5ecfefcfac797fba7dba2d92868ff55b7afb
Andres1410 <camachorodriguezandresfelipe@gmail.com>
2022-11-15 23:26:03 -0500

    modal tailwind

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -2,9 +2,6 @@ import React from "react";
 
 import { Button} from "@material-tailwind/react";
 
-import { LockClosedIcon } from '@heroicons/react/20/solid'
-
-
 export default function Modal() {
   const [showModal, setShowModal] = React.useState(false);
   return (
@@ -29,42 +26,53 @@ export default function Modal() {
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/} 
+                
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <img
                       className="mx-auto h-12 w-auto"
                       src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                       alt="Your Company"
                     />
-                    <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                      Sign in to your account
+                    <h2 className="mt-3 text-center text-3xl font-bold tracking-tight text-gray-900">
+                      Ingresar su cuenta
                     </h2>
                 </div>
 
-                {/*body*/}
-
             <input type="hidden" name="remember" defaultValue="true" />
-                 <div className="-space-y-px rounded-md shadow-sm">
-                  <div>
-                  <label htmlFor="email-address" className="sr-only">
-                      Email address
-                    </label>
-                    <input
-                      id="email-address"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      placeholder="Email address"
-                    />
-                    </div>
+                 
 
+                 <div class="flex justify-between ...">
+                    <div>&nbsp;</div>
+                    <div>
+                      <label htmlFor="email-address" className="sr-only">
+                          Email address
+                        </label>
+                        <input
+                          id="email-address"
+                          name="email"
+                          type="email"
+                          autoComplete="email"
+                          required
+                          className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                          placeholder="Email address"
+                      />
+                    </div>
+                    <div>&nbsp;</div>
+                  </div>
+
+                  <div class="flex justify-between ...">
+                    <div>&nbsp;</div>
+                    <div>&nbsp;</div>
+                    <div>&nbsp;</div>
+                  </div>
+
+                  <div class="flex justify-between ...">
+                    <div>&nbsp;</div>
                     <div>
                       <label htmlFor="password" className="sr-only">
                         Password
-                      </label>
-                      <input
+                        </label>
+                       <input
                         id="password"
                         name="password"
                         type="password"
@@ -72,43 +80,43 @@ export default function Modal() {
                         required
                         className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                         placeholder="Password"
-                      />
-              </div>
+                       />
+                      </div>
+                    <div>&nbsp;</div>
+                  </div>
+
+                  <div class="flex justify-around ...">
+                    <div>&nbsp;</div>
+                    <div>&nbsp;</div>
+                    <div>&nbsp;</div>
+                  </div>
+
+                  <div class="flex justify-evenly ...">
+                    <div>&nbsp;</div>
+                    <div>
+                      <button
+                          type="submit"
+                          className="group relative flex w-min justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2; transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ..."
+                        >
+                          
+                          Ingresar
+                        </button>
+                    </div>
+                    <div>&nbsp;</div>
+                  </div>
+
+                <div class="flex justify-evenly ...">
+                  <div>&nbsp;</div>
+                  <div>
+                     <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                       Olvide contraseña?
+                     </a> 
+                  </div>
+                  <div>&nbsp;</div>
                 </div>
 
-                <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                />
-                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                  Remember me
-                </label>
-              </div>
-
-              <div className="text-sm">
-                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Olvide contraseña?
-                </a>
-              </div>
-            </div>
 
 
-            <div>
-              <button
-                type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-                </span>
-                Ingresar
-              </button>
-            </div>
-                
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
                     className="text-red-500 active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -118,6 +126,7 @@ export default function Modal() {
                     x
                   </button>
                 </div>
+
               </div>
             </div>
           </div>
