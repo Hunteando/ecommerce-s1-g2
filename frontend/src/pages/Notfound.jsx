fca300215876526dc6487c08a84ed254792b4304
Andres1410 <camachorodriguezandresfelipe@gmail.com>
2022-11-17 17:40:26 -0500

    reestructurado sin fondos

--- frontend/src/pages/Notfound.jsx
+++ frontend/src/pages/Notfound.jsx
@@ -1,12 +1,19 @@
 
 import paginanoencontrada from "../assests/images/paginanoencontrada.jpg";
 
+// componente navbar
+import Navbar from "../components/Navbar";
+
 function Notfound() {
   return (
     <>
+    <Navbar />
+
     <div className="flex justify-center">
       <div>
-      <img src={paginanoencontrada} className="bg-origin-content p-4 border-4 border-dashed"/>
+          <img 
+          className=" p-4 border-4 border-none"
+          src={paginanoencontrada} /> 
       </div>
     </div>
     </>
