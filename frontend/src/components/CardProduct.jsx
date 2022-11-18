edd7b1731119e533241310bb706a5eb75266a1ec
Nicolas <nicoc12024@gmail.com>
2022-11-18 17:46:33 +0100

    .

--- frontend/src/components/CardProduct.jsx
+++ frontend/src/components/CardProduct.jsx
@@ -22,20 +22,27 @@ function CardProduct({ data }) {
           </Link>
         </div>
       ) : (
-        <div className="icons absolute text-white flex mt-1 mr-1 right-0 gap-2">
+        <div className="icons absolute right-0 text-white flex m-3 gap-2">
           {showIngredients ? (
-            <BsFillEyeFill
+            <BsFillEyeSlashFill
               size={20}
-              className="text-black"
+              className="text-black z-10"
               onClick={() => setShowIngredients(!showIngredients)}
             />
           ) : (
-            <BsFillEyeSlashFill
+            <BsFillEyeFill
               size={20}
-              className="text-black"
+              className="text-black z-10"
               onClick={() => setShowIngredients(!showIngredients)}
             />
           )}
+        </div>
+      )}
+
+      {showIngredients && (
+        <div className="absolute top-0 left-0 right-0 bottom-0 p-3 bg-#FBD1A1 text-white flex flex-col items-center justify-center">
+          <h4 className="text-sm font-medium mb-4">Ingredientes</h4>
+          <p className="text-xs text-center">{data.ingredients}</p>
         </div>
       )}
 
@@ -45,9 +52,9 @@ function CardProduct({ data }) {
           src={data.image}
           alt="product"
         />
-        <div className="pt-2 text-center">
-          <h4 className="text-sm font-medium uppercase text-blue-600">{data.name}</h4>
-          <p className="mb-2 font-bold">${data.price}</p>
+        <div className="py-2 text-center bg-#F79E38/[.47]">
+          <h4 className="text-sm font-medium uppercase text-#231E1A">{data.name}</h4>
+          <p className="mb-2 font-bold text-#231E1A">${data.price}</p>
           <Counter stock={stock} />
         </div>
       </div>
--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 
-
 function Counter({ stock, onAdd, initial = 1 }) {
   const [count, setCount] = useState(initial);
 
@@ -18,29 +17,29 @@ function Counter({ stock, onAdd, initial = 1 }) {
   };
 
   return (
-    <div className="mt-2">
+    <div>
       <div className="flex justify-center items-center mb-1 ">
         <button
-          className="pb-1 px-3 border-solid border-r-0 border border-gray-500"
+          className="pb-1 px-2 border-solid border-r-0 border border-gray-500"
           onClick={decrease}
         >
           <p>-</p>
         </button>
-        <h1 className="pb-1 px-6 border-solid border-r-0 border border-gray-500">
+        <h1 className="pb-1 px-3 border-solid border-r-0 border border-gray-500">
           {count}
         </h1>
         <button
-          className="pb-1 flex h-auto align-center px-3 border-solid border border-gray-500"
+          className="pb-1 flex h-auto align-center px-2 border-solid border border-gray-500"
           onClick={increase}
         >
           <p>+</p>
         </button>
       </div>
       <button
-        className="w-full flex items-center justify-center py-2 bg-gray-400"
+        className="my-2 mx-auto p-2 rounded flex items-center uppercase  justify-center text-[11px] text-white bg-#C96C26"
         onClick={() => onAdd(count)}
       >
-        <FaShoppingCart className="mr-2" />
+        <FaShoppingCart className="text-white mr-1" size={15} />
         Añadir al carrito
       </button>
     </div>
