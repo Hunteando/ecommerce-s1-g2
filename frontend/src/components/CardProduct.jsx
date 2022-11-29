342eb0d3ebed0316f3b2f98726f496ec58886236
Mariano Labatut <marianolabatut@gmail.com>
2022-11-29 15:06:03 -0300

    Revert "hover on product show ingredients"
    This reverts commit b03cfbef25e3c6e832dccb6fcfcb8d3baf9ad9cb.

--- frontend/src/components/CardProduct.jsx
+++ frontend/src/components/CardProduct.jsx
@@ -49,11 +49,9 @@ function CardProduct({ data }) {
       )}
 
       {showIngredients && (
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-white text-#231E1A">
-          <div className="absolute top-0 bottom-0 bg-#FBD1A1/[0.48] flex flex-col items-center justify-center">
-            <h4 className="text-sm font-medium mb-4">Ingredientes</h4>
-            <p className="text-xs text-center">{data.ingredients}</p>
-          </div>
+        <div className="absolute top-0 left-0 right-0 bottom-0 p-3 bg-#FBD1A1 text-#231E1A flex flex-col items-center justify-center">
+          <h4 className="text-sm font-medium mb-4">Ingredientes</h4>
+          <p className="text-xs text-center">{data.ingredients}</p>
         </div>
       )}
 
